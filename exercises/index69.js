d975a0b592fb8adee07ed62757729d26f63cb138
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-01-29 19:08:05 -0800

    exercise 68, 69, 71

---
+++ exercises/index69.js
@@ -0,0 +1,13 @@
+// # Exercise 69
+
+// * Create a new index69.js file
+// * Use while structure to sum numbers between 0 and 1000
+// * Show the partial result as output too
+
+let number = 0;
+
+while( number <= 99 ) {
+    number++
+    number + number;
+    console.log(number)
+}
---
+++ exercises/index71.js
@@ -0,0 +1,12 @@
+// # Exercise 71
+
+// * Create a new index71.js file
+// * Show the 9 times table from 1 to 10 using while (example 9 times 1 equals 1)
+
+let nine = 9;
+let numberTimes = 0;
+
+while( numberTimes <= 10 ) {
+    numberTimes++; 
+    console.log(nine + " times " + numberTimes + ' equals ' + nine * numberTimes)
+}
