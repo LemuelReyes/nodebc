03a81add1ebea782a53e197467227ae0f48ba5c9
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-01-29 19:27:43 -0800

    exercise 72

---
+++ exercises/index72.js
@@ -0,0 +1,24 @@
+// # Exercise 72
+
+// * Create a new index72.js file
+// * Show the following output using while:
+// ```
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+// ***********
+// ************
+// *************
+
+let starCount = 1;
+while (starCount <= 10){
+    console.log("*".repeat(starCount));
+    starCount++;
+}
