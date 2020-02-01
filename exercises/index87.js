312a78da4de7354ab9554b92bdf81e2715efcbb6
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-01-31 19:24:54 -0800

    exercise 98

--- exercises/index87.js
+++ exercises/index87.js
@@ -1,5 +1,13 @@
-let number = 0;
+// let number = 0;
 
-for (number = 0; number < 101; number++) {
-    console.log(number)
-}
+// for (number = 0; number < 101; number++) {
+//     console.log(number)
+// }
+
+for (let index = 0; index < 1000; index++){
+    if (index < 10) {
+      console.log(index);
+    } else {
+      break;
+    }
+  }
---
+++ exercises/index98.js
@@ -0,0 +1,14 @@
+// # Exercise 98
+
+// * Create a new index98.js file
+// * Add all the numbers bettwen 0 and 1000 using for statement
+// * The iteration must break if the partial result is bigger than 400
+// * Show the result as output
+
+for (let number = 0; number < 1001; number++)  {
+    if (number < 400) {
+        console.log(number);
+    } else {
+        break;
+    }
+}
