d416a37e7df4a4ac267572567256a82d327c7fdd
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-01-31 19:07:07 -0800

    exercise 82

---
+++ exercises/index82.js
@@ -0,0 +1,12 @@
+// # Exercise 82
+
+// * Create a new index82.js file
+// * Copy and paste the code from exercise 72
+// * Refactor the code to use do/while instead of while
+
+let starCount = 1;
+
+do {
+    console.log("*".repeat(starCount));
+    starCount++;
+} while (starCount <= 10)
