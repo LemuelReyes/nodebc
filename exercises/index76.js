1cfefb6a37b435a7288021b4a90e8c580777fc05
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-01-31 18:48:42 -0800

    exercise 76

---
+++ exercises/index76.js
@@ -0,0 +1,14 @@
+// # Exercise 77
+
+// * Create a new index77.js file
+// * Copy and paste the code from exercise 67
+// * Refactor the code to use do/while instead of while
+
+let number = 0;
+
+do {
+    console.log(number)
+    number++;
+} while (number < 101) {
+    
+}
