d9b21fb1462339a7158304db1a7f610f4e0cb224
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-01-31 18:56:02 -0800

    add exercise 78

---
+++ exercises/index78.js
@@ -0,0 +1,12 @@
+// # Exercise 78
+
+// * Create a new index78.js file
+// * Copy and paste the code from exercise 68
+// * Refactor the code to use do/while instead of while
+
+let number = 0;
+
+do {
+    number += 2;
+    console.log(number);
+} while ( number <= 100 ) 
