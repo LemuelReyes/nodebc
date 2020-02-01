36bacaea13ce4e73e93b5e713fd685d286eb26da
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-01-31 19:11:10 -0800

    exercise 86

---
+++ exercises/index86.js
@@ -0,0 +1,12 @@
+// # Exercise 86
+
+// * Create a new index86.js file
+// * Copy and paste the code from exercise 76
+// * Refactor the code to use for instead of do/while
+
+
+let number = 0;
+
+for ( let number = 0; number < 11; number++) {
+    console.log(number)
+}
