b0664a5cbb6094dd02f34a212ba43f4111c0c72a
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-01-31 19:05:08 -0800

    exercise 81

---
+++ exercises/index81.js
@@ -0,0 +1,13 @@
+// # Exercise 81
+
+// * Create a new index81.js file
+// * Copy and paste the code from exercise 71
+// * Refactor the code to use do/while instead of while
+
+let nine = 9;
+let numberTimes = 0;
+
+do {
+    console.log(nine + " times " + numberTimes + ' equals ' + nine * numberTimes)
+    numberTimes++;
+} while( numberTimes <= 10 ) 
