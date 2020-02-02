f50316acb004ec2a13797ec06521ccfeeeb7deaa
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-02-02 13:30:15 -0800

    exercise 117

---
+++ exercises/index117.js
@@ -0,0 +1,11 @@
+// # Exercise 117
+
+// * Create a new index117.js file
+// * Define a students array and add 5 class students names as students array items
+// * Show the first, third and fifth student name as output
+
+const students = ['Lemuel', 'Brian', 'Keyosha', 'Raymond', 'Juliani']
+
+const filterStudents = [students[0], students[2], students[4]];
+
+console.log(filterStudents)
