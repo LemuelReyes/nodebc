39c6db11fa42aab97359a91ba3701a3bfca5edf8
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-02-02 18:38:56 -0800

    exercise 136

---
+++ exercises/index136.js
@@ -0,0 +1,34 @@
+// x# Exercise 136
+
+// * Create a new index136.js file
+// * Define a mutants array and assign the following values
+
+// ```
+// Professor X
+// Cyclops
+// Iceman
+// Angel
+// Magneto
+// Beast
+// Phoenix
+// Logan
+// Gambit
+// ```
+
+// * Iterate over each mutants item using forEach
+// * The objective is to create a string value with Iceman, Logan and Phoenix
+// * To create the string you need to get the names while iterating the mutants array
+// * Once the iteration is over show the generated string with the selected mutants names
+
+let mutants = ['Professor X', 'Cyclops', 'Iceman', 'Angel', 'Magneto', 'Beast', 'Phoenix', 'Logan', 'Gambit']
+
+mutants.forEach(function(mutantNames){
+
+    const filteredMutants = [];
+
+    if(mutantNames.includes('Iceman', 'Logan', 'Phoenix')) {
+        filteredMutants.push('Iceman', 'Logan', 'Phoenix' )
+        console.log(filteredMutants)
+    } 
+})
+
