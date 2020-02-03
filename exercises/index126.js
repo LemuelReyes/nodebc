531d3e1fe747db02ebc0421ff8ec1be02eb09b9b
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-02-02 16:12:26 -0800

    add exercise 126. using unshift method

---
+++ exercises/index126.js
@@ -0,0 +1,12 @@
+// # Exercise 126
+
+// * Create a new index126.js file
+// * Define a iceCreamFlavors array
+// * Add 10 flavors that you like one by one using the unshift method
+// * Show the iceCreamFlavors items as output
+
+const iceCreamFlavors = ['Chocolate', 'Vanilla', 'Mint']
+
+const addNewFlavor = iceCreamFlavors.unshift('Rocky Road', 'Ferrero')
+
+console.log(iceCreamFlavors)
