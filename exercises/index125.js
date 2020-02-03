5afa92b2c262ef5fd7b8ede22286ba9d9408e71f
Lemuel Reyes <reyeslemuel@hotmail.com>
2020-02-02 16:07:53 -0800

    add exercise 125

---
+++ exercises/index125.js
@@ -0,0 +1,12 @@
+// # Exercise 125
+
+// * Create a new index125.js file
+// * Define a iceCreamFlavors array
+// * Add 10 flavors that you like one by one using the push method
+// * Show the iceCreamFlavors items as output
+
+const iceCreamFlavors = ['Chocolate', 'Vanilla', 'Strawberry']
+
+const addNewFlavor = iceCreamFlavors.push('Pecan')
+
+console.log(iceCreamFlavors)
